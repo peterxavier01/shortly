26ac7e164d2c66de6780a922be2ce757f0ab7de6
Peter Uadiale <fxavy02@gmail.com>
2024-05-12 00:38:09 +0100

    refactor: wrap the app with the lazy motion provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+
 import App from "./App.tsx";
 import { StateProvider } from "./context/StateContext.tsx";
+import MotionLazy from "./components/MotionLazy.tsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <StateProvider>
-      <App />
+      <MotionLazy>
+        <Toaster />
+        <App />
+      </MotionLazy>
     </StateProvider>
   </React.StrictMode>
 );
