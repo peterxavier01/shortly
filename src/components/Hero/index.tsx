7b2c89b6c54d7e726c8fc5018b948324c0b86654
Peter Uadiale <fxavy02@gmail.com>
2024-05-12 00:40:58 +0100

    refactor: add animations to the hero section

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,24 +1,33 @@
+import { m } from "framer-motion";
+
 import Button from "../Button";
 
 import HeroImg from "../../assets/illustration-working.svg";
+
+import { heroImgVariants, heroTextVariants } from "../../lib/animations";
 
 import classes from "./index.module.css";
 
 const Hero = () => {
   return (
     <section className={classes.hero}>
-      <div>
+      <m.div variants={heroTextVariants} initial="hidden" animate="visible">
         <h1>More than just shorter links</h1>
         <p>
           Build your brand's recognition and get detailed insights on how your
           links are performing
         </p>
         <Button>Get Started</Button>
-      </div>
-      
-      <div className={classes.imgContainer}>
+      </m.div>
+
+      <m.div
+        variants={heroImgVariants}
+        initial="hidden"
+        animate="visible"
+        className={classes.imgContainer}
+      >
         <img src={HeroImg} alt="hero" />
-      </div>
+      </m.div>
     </section>
   );
 };
